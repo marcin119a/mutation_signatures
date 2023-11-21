a4e21277cf5231ba6263eb4628e906f63b08c8b0
amso <marcin119a@gmail.com>
2023-11-21 11:59:43 +0100

    bootstrapSig Exposures comment

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import numpy as np
 
 if __name__ == '__main__':
     tumorBRCA = np.genfromtxt('data/tumorBRCA.csv', delimiter=',', skip_header=1)
+    patients = np.genfromtxt('data/tumorBRCA.csv', delimiter=',', max_rows=1, dtype=str)[1:]
     tumorBRCA = np.delete(tumorBRCA, 0, axis=1)
     signaturesCOSMIC = np.genfromtxt('data/signaturesCOSMIC.csv', delimiter=',', skip_header=1)
     signaturesCOSMIC = np.delete(signaturesCOSMIC, 0, axis=1)
     first_col = tumorBRCA[:, 0]
     #res = decomposeQP(first_col, signaturesCOSMIC)
 
-    #exposures, errors = findSigExposures(tumorBRCA, signaturesCOSMIC, decomposition_method=decomposeQP)
+    exposures, errors = findSigExposures(tumorBRCA, signaturesCOSMIC, decomposition_method=decomposeQP)
     #print(exposures, errors)
     #print(exposures.shape, errors.shape)
-    #np.savetxt('output/bootstrap_exposures.csv', exposures, delimiter=',')
+    np.savetxt('output/exposures.csv', exposures, delimiter=',', header=','.join(patients))
 
-    #np.savetxt('output/bootstrap_errors.csv', errors, delimiter=',')
+    np.savetxt('output/errors.csv', errors, delimiter=',')
 
     #exposures, errors = bootstrapSigExposures(first_col, signaturesCOSMIC, 100, 2000)
     num_folds = 5

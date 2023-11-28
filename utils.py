e30700fb728c217845e77c635027c12256f1c411
amso <marcin119a@gmail.com>
2023-11-28 12:29:37 +0100

    poc BIC calculation

--- utils.py
+++ utils.py
@@ -9,3 +9,12 @@ def is_wholenumber(x, tol=1e-15):
     return np.abs(x - np.round(x)) < tol
 
 
+def calculate_BIC(M, exposures, errors):
+    n = M.shape[1]  # Number of patients
+    k = exposures.shape[0]  # Number of signatures
+    RSS = np.sum(errors**2)
+
+    log_likelihood = -n/2 * np.log(RSS)
+    BIC = k * np.log(n) - 2 * log_likelihood
+
+    return BIC
